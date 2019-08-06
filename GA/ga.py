b2c0d7450d11eaf0fc6c821f4c843eb6f25ae110
RKouhei <kohriki222@gmail.com>
2019-08-06 22:23:54 +0900

    bug fixed

--- GA/ga.py
+++ GA/ga.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import copy
 # MAXW = 9   # 最大重量
 # MINW = 1   # 最小重量
 # MAXV = 10  # 最大価値
@@ -32,7 +32,6 @@ class ga :
         self.fbest = max(self.f) # 郡全体の最適解(評価関数)(初期化)
         self.xbest = self.x[np.argmax(self.f)] # 郡全体の最適解(染色体)(初期化)
 
-
     def calc_value(self) :
         for i in range(self.m) :
             self.f[i] = 0
@@ -44,7 +43,7 @@ class ga :
                     self.f[i] += self.value[j]
                     tmpw += self.weight[j]
             if tmpw > self.wmax :
-                self.f[i] = 1
+                self.f[i] = 1 
 
     def choice_parent(self) : # 親選択
         sumf = self.f / sum(self.f)
@@ -63,7 +62,7 @@ class ga :
             self.calc_value() # fの更新
             if max(self.f) > self.fbest :
                 self.fbest = max(self.f) # fbestの更新
-                self.xbest = self.x[np.argmax(self.f)] # xbestの更新
+                self.xbest = copy.copy(self.x[np.argmax(self.f)]) # xbestの更新
             for i in range(0,self.m) :
                 self.two_point_crossing(i) # 2点交叉
                 # 突然変異
@@ -76,23 +75,36 @@ if __name__ == '__main__' :
     # value = (MAXV - MINV) * np.random.randn(d) + MINV # 各価値をランダムで
 
     # weight = np.array([7,5,1,9,6]) # 品物5個
-    weight = np.array([3,6,5,4,8,5,3,4,8,2]) # 品物5個
-    # value = np.array([50,40,10,70,55]) # 品物10個
+    weight = np.array([3,6,5,4,8,5,3,4,8,2]) # 品物10個
+    # value = np.array([50,40,10,70,55]) # 品物5個
     value = np.array([70,120,90,70,130,80,40,50,30,70]) # 品物10個
 
     m = 20 # 個体数
     d = len(weight)  # 解の次元数もとい品物数
     cnt = 0 # カウント
 
+    suit_flag = True
+    suit_value = 0
     for i in range(100) :
         ga1 = ga(m,d,weight,value) # イニシャライズ
         ga1.calc() # 最適化
-        if int(ga1.fbest) == 420 :
+
+        # 最適解は?
+        if suit_value < ga1.fbest :
+            suit_value = ga1.fbest
+
+        # 最適解を何回求めるか
+        # if int(ga1.fbest) == 125 : # 品物5個
+        print(ga1.fbest)
+        if int(ga1.fbest) == 420 : # 品物10個
             cnt += 1
-        if i % 10 == 0 :
-            print(f'{i} is done. cnt = {cnt}')
+            if suit_flag :
+                xb = ga1.xbest
+                suit_flag = False
+        # if i % 10 == 0 :
+            # print(f'{i} is done. cnt = {cnt}')
 
     print(f'最適解を求めた回数{cnt}')
     # print(f"終了時刻t = {ga1.stopt}")
-    # print(f"解の目的関数値Fg = {ga1.fbest}")
-    # print(f'最適解 Xbest = {ga1.xbest}')
+    print(f"最適値 Fg = {suit_value}")
+    print(f'最適解 Xbest = {xb}')
